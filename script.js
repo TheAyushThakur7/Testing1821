84c75767661e64280ce32e32d453bc43c249a219
TheAyushThakur7 <goodcontent1010@gmail.com>
2025-03-23 04:53:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
         menu.classList.add("active");
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".platform-tab span").forEach(tab => {
+        tab.addEventListener("click", function() {
+            // Remove active class from the previously selected tab
+            document.querySelector(".platform-tab .active").classList.remove("active");
+            this.classList.add("active");
+
+            // Get the correct image based on the selected tab
+            let platform = this.dataset.platform;
+            let screenshot = document.getElementById("showcase-image");
+
+            if (screenshot) {
+                if (platform === "Instagram") {
+                    screenshot.src = "1.jpg";  
+                } else if (platform === "TikTok") {
+                    screenshot.src = "2.jpg";
+                } else if (platform === "YouTube") {
+                    screenshot.src = "3.jpg";
+                }
+            }
+        });
+    });
+});
